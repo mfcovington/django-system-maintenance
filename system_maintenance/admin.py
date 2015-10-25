897d7e7c2ac5da2e7c1f93a454e66aada8a9917d
Mike Covington <mfcovington@gmail.com>
2015-10-24 17:05:21 -0700

    Register basic admins for system maintenance models

--- system_maintenance/admin.py
+++ system_maintenance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Maintenance, MaintenanceType, Software, SysAdmin, System
+
+
+admin.site.register(Maintenance)
+admin.site.register(MaintenanceType)
+admin.site.register(Software)
+admin.site.register(SysAdmin)
+admin.site.register(System)
