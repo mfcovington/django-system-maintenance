29787037ed7a0858ae66e1f7bee07803366e4e42
Mike Covington <mfcovington@gmail.com>
2015-12-02 18:59:39 -0800

    After manually logging out, redirect back to '/system_maintenance/'

--- system_maintenance/urls.py
+++ system_maintenance/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^authentication/$', 'django.contrib.auth.views.login', {'template_name': 'system_maintenance/authentication.html'}, name='authentication'),
     url(r'^documentation/$', DocumentationRecordListView.as_view(), name='documentation_record_list'),
     url(r'^documentation/(?P<pk>\d+)/$', DocumentationRecordDetailView.as_view(), name='documentation_record_detail'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/system_maintenance/'}, name='logout'),
     url(r'^records/$', MaintenanceRecordListView.as_view(), name='maintenance_record_list'),
     url(r'^records/(?P<pk>\d+)/$', MaintenanceRecordDetailView.as_view(), name='maintenance_record_detail'),
 )
