d46d0b5a5392b6ca047b519a9d6280b5b0581e81
Mike Covington <mfcovington@gmail.com>
2015-12-26 00:32:46 -0800

    Switch to 'StaticLiveServerTestCase' to avoid having to set 'settings.STATIC_ROOT'

--- system_maintenance/tests/functional/tests.py
+++ system_maintenance/tests/functional/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 
 
-class FunctionalTest(LiveServerTestCase):
+class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
