9a5e716fd4029ea11ab8638c0cb6c72a6fa4ab5b
Mike Covington <mfcovington@gmail.com>
2015-12-31 00:31:43 -0800

    Test that the color coding for 'Complete', 'In Progress', and 'Failed' statuses are correct

--- system_maintenance/tests/functional/tests.py
+++ system_maintenance/tests/functional/tests.py
@@ -2,8 +2,10 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.color import Color
 
-from system_maintenance.tests.utilities import populate_test_db
+from system_maintenance.tests.utilities import (
+    login_sysadmin_user, populate_test_db)
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -164,3 +166,22 @@ class FunctionalTest(StaticLiveServerTestCase):
             self.password_inputbox.size['width'] / 2
         self.assertAlmostEqual(center_username, window_width / 2, delta=5)
         self.assertAlmostEqual(center_password, window_width / 2, delta=5)
+
+
+        # Sign in as sysadmin and go to Maintenance Records
+        self.browser.get(self.system_maintenance_url('records'))
+        self.find_authentication_elements()
+        self.username_inputbox.send_keys('sysadmin')
+        self.password_inputbox.send_keys('sysadmin' + Keys.ENTER)
+
+        # See, based on the color-coded backgrounds, that the status of the
+        # first record is 'Failed', the second is 'Complete', and the third is
+        # 'In Progress'
+        list_group_items = self.browser.find_elements_by_class_name(
+            'list-group-item')
+        background_colors = ['#f2dede', '#ffffff', '#fcf8e3']
+
+        self.assertEqual(len(list_group_items), 3)
+        for item, color in zip(list_group_items, background_colors):
+            self.assertEqual(Color.from_string(
+                item.value_of_css_property('background-color')).hex, color)
