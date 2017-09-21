d8494057cd9ab0c9133257a22f61135f60e35556
Mike Covington <mfcovington@gmail.com>
2017-09-21 10:53:19 -0700

    Perform sysadmin check before showing raw records

--- system_maintenance/views.py
+++ system_maintenance/views.py
@@ -12,25 +12,6 @@ from .models import (DocumentationRecord, Hardware, MaintenanceRecord,
     MaintenanceType, Software, SysAdmin, System)
 
 
-def raw_view(request, *args, **kwargs):
-    if kwargs['type_of_record'] == 'documentation':
-        record = DocumentationRecord.objects.get(pk=kwargs['record_pk'])
-    elif kwargs['type_of_record'] == 'maintenance':
-        record = MaintenanceRecord.objects.get(pk=kwargs['record_pk'])
-    else:
-        pass
-
-    field = getattr(record, kwargs['type_of_field'])
-
-    context = {
-        'type_of_field': kwargs['type_of_field'],
-        'raw': field.raw,
-        'record': record,
-    }
-    return render(
-        request, 'system_maintenance/raw.html', context)
-
-
 def sysadmin_check(user):
     """
     Check whether user is a sysadmin and has an active account.
@@ -56,6 +37,28 @@ class SysAdminRequiredMixin(object):
         login_url=reverse_lazy('system_maintenance:authentication')))
     def dispatch(self, *args, **kwargs):
         return super(SysAdminRequiredMixin, self).dispatch(*args, **kwargs)
+
+
+@user_passes_test(
+    sysadmin_check,
+    login_url=reverse_lazy('system_maintenance:authentication'))
+def raw_view(request, *args, **kwargs):
+    if kwargs['type_of_record'] == 'documentation':
+        record = DocumentationRecord.objects.get(pk=kwargs['record_pk'])
+    elif kwargs['type_of_record'] == 'maintenance':
+        record = MaintenanceRecord.objects.get(pk=kwargs['record_pk'])
+    else:
+        pass
+
+    field = getattr(record, kwargs['type_of_field'])
+
+    context = {
+        'type_of_field': kwargs['type_of_field'],
+        'raw': field.raw,
+        'record': record,
+    }
+    return render(
+        request, 'system_maintenance/raw.html', context)
 
 
 @user_passes_test(
