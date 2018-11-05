c0d2b3d052d8c4dd7afce1665119eef847dcae59
Mike Covington <mfcovington@gmail.com>
2018-11-05 08:09:31 -0800

    Use Django 2 pattern for urls.py

--- system_maintenance/urls.py
+++ system_maintenance/urls.py
@@ -1,20 +1,18 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import (DocumentationRecordListView, DocumentationRecordDetailView,
-    MaintenanceRecordDetailView, MaintenanceRecordListView, raw_view,
-    system_maintenance_home_view)
+from . import views
 
 
 app_name = 'system_maintenance'
 
 urlpatterns = [
-    url(r'^$', system_maintenance_home_view, name='system_maintenance_home_view'),
-    url(r'^authentication/$', auth_views.LoginView.as_view(), {'template_name': 'system_maintenance/authentication.html'}, name='authentication'),
-    url(r'^documentation/$', DocumentationRecordListView.as_view(), name='documentation_record_list'),
-    url(r'^documentation/(?P<pk>\d+)/$', DocumentationRecordDetailView.as_view(), name='documentation_record_detail'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/system_maintenance/'}, name='logout'),
-    url(r'^raw/(?P<type_of_record>[A-Za-z]+)/(?P<type_of_field>[A-Za-z]+)/(?P<record_pk>\d+)/$', raw_view, name='raw_view'),
-    url(r'^records/$', MaintenanceRecordListView.as_view(), name='maintenance_record_list'),
-    url(r'^records/(?P<pk>\d+)/$', MaintenanceRecordDetailView.as_view(), name='maintenance_record_detail'),
+    path('', views.system_maintenance_home_view, name='system_maintenance_home_view'),
+    path('authentication/', auth_views.LoginView.as_view(template_name='system_maintenance/authentication.html'), name='authentication'),
+    path('documentation/', views.DocumentationRecordListView.as_view(), name='documentation_record_list'),
+    path('documentation/<int:pk>/', views.DocumentationRecordDetailView.as_view(), name='documentation_record_detail'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/system_maintenance/'), name='logout'),
+    path('raw/<type_of_record>/<type_of_field>/<int:record_pk>/', views.raw_view, name='raw_view'),
+    path('records/', views.MaintenanceRecordListView.as_view(), name='maintenance_record_list'),
+    path('records/<int:pk>/', views.MaintenanceRecordDetailView.as_view(), name='maintenance_record_detail'),
 ]
