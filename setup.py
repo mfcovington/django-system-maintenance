e4bee615541bea1318f05f0a8507d0db61eb437d
Mike Covington <mfcovington@gmail.com>
2018-11-04 21:26:20 -0800

    Add Django 1.9 to list of supported versions

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
